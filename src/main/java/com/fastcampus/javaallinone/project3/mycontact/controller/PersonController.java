146315070c904b60c6a16626465dce5b86eaefcb
ANGONGIL <qws458@naver.com>
2020-08-10 14:39:31 +0900

    PersonController에 ExceptionHandler를 추가

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/controller/PersonController.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/controller/PersonController.java
@@ -2,13 +2,20 @@ package com.fastcampus.javaallinone.project3.mycontact.controller;
 
 import com.fastcampus.javaallinone.project3.mycontact.controller.dto.PersonDto;
 import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
+import com.fastcampus.javaallinone.project3.mycontact.exception.PersonNotFoundException;
+import com.fastcampus.javaallinone.project3.mycontact.exception.RenameNotPermittedException;
+import com.fastcampus.javaallinone.project3.mycontact.exception.dto.ErrorResponse;
 import com.fastcampus.javaallinone.project3.mycontact.repository.PersonRepository;
 import com.fastcampus.javaallinone.project3.mycontact.service.PersonService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.ws.Response;
+
+@Slf4j
 @RequestMapping(value = "/api/person")
 @RestController
 public class PersonController {
@@ -25,13 +32,11 @@ public class PersonController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void postPerson(@RequestBody PersonDto personDto){
-        personService.put(personDto);
-
+            personService.put(personDto);
     }
     @PutMapping("/{id}")
     public void modifyPerson(@PathVariable Long id,@RequestBody PersonDto personDto){
-        personService.modify(id,personDto);
-
+            personService.modify(id,personDto);
     }
     @PatchMapping("/{id}")
     public void modifyName(@PathVariable Long id, String name){
@@ -42,4 +47,19 @@ public class PersonController {
         personService.delete(id);
 
     }
+    @ExceptionHandler(value = RenameNotPermittedException.class)
+    public ResponseEntity<ErrorResponse> handleRenameNoPermittedException(RenameNotPermittedException ex){
+        return new ResponseEntity<>(ErrorResponse.of(HttpStatus.BAD_REQUEST,ex.getMessage()),HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(value = PersonNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handlePersonNotFoundException(PersonNotFoundException ex){
+        return new ResponseEntity<>(ErrorResponse.of(HttpStatus.BAD_REQUEST,ex.getMessage()),HttpStatus.BAD_REQUEST);
+
+    }
+    @ExceptionHandler(value = RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex){
+        log.error("서버 오류 : {}",ex.getMessage(),ex);
+        return new ResponseEntity<>(ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR,"알 수 없는 서버 오류가 발생하였습니다."),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
